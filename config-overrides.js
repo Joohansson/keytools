5a291f4654f950b394280e0c39255e2fe2d7da2c
Joohansson <d84@outlook.com>
2019-12-30 15:59:48 +0100

    Mnemonic and ledger support

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 module.exports = function override(config, env) {
+  //allow the use of web workers in chrome when running from a file system by using inline: true
   config.module.rules.push({
       test: /\.worker\.js$/,
       use: {
@@ -8,6 +9,8 @@ module.exports = function override(config, env) {
         }
       }
     })
+  //ignore bip39 word lists that are not english. TODO: Make webpack work with non-ejected react
+  //config.plugins.push( new webpack.IgnorePlugin(/^\.\/(?!english)/, /bip39\/src\/wordlists$/) );
   return config;
 
   //just return the original config and pretend this never happened
