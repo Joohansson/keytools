0c9261b1394074433c2a7cc85e6d47fb106b4f90
Joohansson <d84@outlook.com>
2020-02-02 07:26:51 +0100

    A bit faster

--- src/tools/SeedTool.js
+++ src/tools/SeedTool.js
@@ -301,12 +301,12 @@ class SeedTool extends Component {
       this.inputToast = toast("Mnemonic not 24 words", helpers.getToast(helpers.toastType.ERROR_AUTO))
       return
     }
-    let nanowallet = wallet.generate(seed)
     var privKey
     if (this.state.selectedDerivationMethod === '0') {
       privKey = nano_old.deriveSecretKey(seed, index)
     }
     else {
+      let nanowallet = wallet.generate(seed)
       let accounts = wallet.accounts(nanowallet.seed, index, index)
       privKey = accounts[0].privateKey
     }
